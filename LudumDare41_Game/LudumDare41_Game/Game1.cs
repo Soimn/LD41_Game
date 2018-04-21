9a4d640ad17a3ccd2c51f764f762448496a1604a
username6 <olah.jac@gmail.com>
2018-04-21 20:06:04 +0200

    Added pause function on key ESC

--- LudumDare41_Game/LudumDare41_Game/Game1.cs
+++ LudumDare41_Game/LudumDare41_Game/Game1.cs
@@ -14,7 +14,10 @@ namespace LudumDare41_Game {
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
 
-        enum GameStates { MENU, INGAME, PAUSE }; //gamestates, legg til om vi trenger
+        KeyboardState oldState;
+
+        enum GameStates { MENU, INGAME}; //gamestates, legg til om vi trenger
+        bool isPaused = false;
         GameStates currentState = GameStates.INGAME;
 
         private Camera2D camera;
@@ -84,60 +87,68 @@ namespace LudumDare41_Game {
         }
 
         protected override void Update (GameTime gameTime) {
-            if (Keyboard.GetState().IsKeyDown(Keys.Escape))
-                Exit();
-            
-            switch (currentState) {
-                case GameStates.MENU: //vente med denne til slutt
-                    break;
-
-                case GameStates.INGAME:
-                    var deltaSeconds = (float)gameTime.ElapsedGameTime.TotalSeconds;
-                    var keyboardState = Keyboard.GetState();
-
-                    const float cameraSpeed = 500f;
-
-                    var moveDirection = Vector2.Zero;
-
-                    if (keyboardState.IsKeyDown(Keys.W) || keyboardState.IsKeyDown(Keys.Up))
-                        moveDirection -= Vector2.UnitY;
-
-                    if (keyboardState.IsKeyDown(Keys.A) || keyboardState.IsKeyDown(Keys.Left))
-                        moveDirection -= Vector2.UnitX;
-
-                    if (keyboardState.IsKeyDown(Keys.S) || keyboardState.IsKeyDown(Keys.Down))
-                        moveDirection += Vector2.UnitY;
-
-                    if (keyboardState.IsKeyDown(Keys.D) || keyboardState.IsKeyDown(Keys.Right))
-                        moveDirection += Vector2.UnitX;
-
-                    if (moveDirection != Vector2.Zero) {
-                        moveDirection.Normalize();
-                        camera.Move(moveDirection * cameraSpeed * deltaSeconds);
-                    }
-
-                    level01.Update(gameTime);
-
-
-                    gui.Update(gameTime, Window, camera);
-                    cards.Update(gameTime, Window);
-
-                    #region // Towers //
-
-                    if (Keyboard.GetState().IsKeyDown(Keys.LeftAlt) && lastTowerSpawn > towerSpawnCooldown) {
-                        towerManager.SpawnTower(new MageTower(towerManager, contentManager), new CoordinateSystem.TileCoord(UI.WorldSelector.selectedTile.X, UI.WorldSelector.selectedTile.Y));
-                        lastTowerSpawn = 0;
-                    }
-
-                    towerManager.Update(gameTime);
-
-                    lastTowerSpawn += (float)gameTime.ElapsedGameTime.TotalSeconds;
-
-                    #endregion
-                    break;
+            KeyboardState newState = Keyboard.GetState();
+            if(newState.IsKeyDown(Keys.Escape) && oldState.IsKeyUp(Keys.Escape)) { 
+                if (isPaused) {
+                    isPaused = false;
+                }
+                else {
+                    isPaused = true;
+                }
             }
 
+            if (!isPaused) {
+                switch (currentState) {
+                    case GameStates.MENU: //vente med denne til slutt
+                        break;
 
+                    case GameStates.INGAME:
+                        var deltaSeconds = (float)gameTime.ElapsedGameTime.TotalSeconds;
+                        var keyboardState = Keyboard.GetState();
+
+                        const float cameraSpeed = 500f;
+
+                        var moveDirection = Vector2.Zero;
+
+                        if (keyboardState.IsKeyDown(Keys.W) || keyboardState.IsKeyDown(Keys.Up))
+                            moveDirection -= Vector2.UnitY;
+
+                        if (keyboardState.IsKeyDown(Keys.A) || keyboardState.IsKeyDown(Keys.Left))
+                            moveDirection -= Vector2.UnitX;
+
+                        if (keyboardState.IsKeyDown(Keys.S) || keyboardState.IsKeyDown(Keys.Down))
+                            moveDirection += Vector2.UnitY;
+
+                        if (keyboardState.IsKeyDown(Keys.D) || keyboardState.IsKeyDown(Keys.Right))
+                            moveDirection += Vector2.UnitX;
+
+                        if (moveDirection != Vector2.Zero) {
+                            moveDirection.Normalize();
+                            camera.Move(moveDirection * cameraSpeed * deltaSeconds);
+                        }
+
+                        level01.Update(gameTime);
+
+
+                        gui.Update(gameTime, Window, camera);
+                        cards.Update(gameTime, Window);
+
+                        #region // Towers //
+
+                        if (Keyboard.GetState().IsKeyDown(Keys.LeftAlt) && lastTowerSpawn > towerSpawnCooldown) {
+                            towerManager.SpawnTower(new MageTower(towerManager, contentManager), new CoordinateSystem.TileCoord(UI.WorldSelector.selectedTile.X, UI.WorldSelector.selectedTile.Y));
+                            lastTowerSpawn = 0;
+                        }
+
+                        towerManager.Update(gameTime);
+
+                        lastTowerSpawn += (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+                        #endregion
+                        break;
+                }
+            }
+            oldState = newState;
             base.Update(gameTime);
         }
 
@@ -168,6 +179,11 @@ namespace LudumDare41_Game {
 
 
                     spriteBatch.DrawString(debugFont, "FPS: " + (Math.Round(1000/gameTime.ElapsedGameTime.TotalMilliseconds)).ToString(), new Vector2(0, 0), Color.Black); //FPS Counter
+
+                    if (isPaused) {
+                        string pausedMsg = "Game paused, press ESC to resume.";
+                        spriteBatch.DrawString(debugFont, pausedMsg, new Vector2(Window.ClientBounds.Width / 2 - debugFont.MeasureString(pausedMsg).X / 2, Window.ClientBounds.Height / 2), Color.Black);
+                    }
 
                     spriteBatch.End();
                     break;
