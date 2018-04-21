fc35a739054f784cb125b2deba2769016d782239
username6 <olah.jac@gmail.com>
2018-04-21 13:18:42 +0200

    Cards v1

--- LudumDare41_Game/LudumDare41_Game/UI/Cards.cs
+++ LudumDare41_Game/LudumDare41_Game/UI/Cards.cs
@@ -1,16 +1,33 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace LudumDare41_Game.UI {
+
     class Cards {
+
     }
 
     class Card {
-        public Card() { //dette er de forskjellige Card typene, som kan bli håndtert i Cards classen?
+        string title, desc, imgNameSrc, bgNameSrc;
+        Texture2D cardImg, bg;
+        bool isLoaded { get; }
 
+        public Card(string title, string desc, string imgNameSrc, string bgNameSrc) { //dette er de forskjellige Card typene, som kan bli håndtert i Cards classen?
+            this.title = title;
+            this.desc = desc;
+            this.imgNameSrc = imgNameSrc;
+            this.bgNameSrc = bgNameSrc;
+            isLoaded = false;
+        }
+
+        public void Load(ContentManager c) {
+            cardImg = c.Load<Texture2D>("GUI/Cards/" + title + "/" + imgNameSrc);
+            bg = c.Load<Texture2D>("GUI/Cards/" + title + "/" + bgNameSrc);
         }
     }
 }
